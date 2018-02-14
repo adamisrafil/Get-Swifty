09599e1187bf5888488dd9c7a4a3238b610d4531
Unknown <adam.israfil@gmail.com>
2018-02-13 23:07:40 -0500

    Connected UI Buttons to Code
    Decided to be a A+ student and added an additional division button not in the lecture

--- Calculator/Calculator/ViewController.swift
+++ Calculator/Calculator/ViewController.swift
@@ -8,7 +8,19 @@
 
 import UIKit
 
+enum modes {
+    case not_set
+    case addition
+    case subtraction
+}
+
 class ViewController: UIViewController {
+    @IBOutlet weak var label: UILabel!
+    
+    var labelString:String = "0"
+    var currentMode:modes = .not_set
+    var savedNum:Int = 0
+    var lastButtonWasMode:Bool = false
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -18,6 +30,42 @@ class ViewController: UIViewController {
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func didPressPlus(_ sender: Any) {
+        
+    }
+    
+    @IBAction func didPressSubtract(_ sender: Any) {
+        
+    }
+    
+    @IBAction func didPressMultiply(_ sender: Any) {
+        
+    }
+    
+    @IBAction func didPressDivide(_ sender: Any) {
+        
+    }
+    
+    @IBAction func didPressEquals(_ sender: Any) {
+        
+    }
+    
+    @IBAction func didPressClear(_ sender: Any) {
+        
+    }
+    
+    @IBAction func didPressNumber(_ sender: UIButton) {
+        
+    }
+    
+    func updateText() {
+        
+    }
+    
+    func changeModes(newMode:modes){
+        
     }
 
 
