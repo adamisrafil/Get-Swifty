ec868c68dd0bfd01d121a9aa673fe715ea3d9152
Unknown <adam.israfil@gmail.com>
2018-02-15 12:34:55 -0500

    Basic Practice with building an app

---
+++ Learning/basic/basic/ViewController.swift
@@ -0,0 +1,31 @@
+//
+//  ViewController.swift
+//  basic
+//
+//  Created by Adam Israfil on 2/15/18.
+//  Copyright © 2018 Adam Israfil. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    @IBOutlet weak var myLabel: UILabel!
+    
+    @IBAction func didTapLoad(_ sender: Any) {
+        myLabel.text = "Changed with code"
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
