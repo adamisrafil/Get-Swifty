dfe51350f0259c7937d6bf590238788dba34a9a0
Unknown <adam.israfil@gmail.com>
2018-02-22 13:24:37 -0500

    Day 3 start: working with picker views

---
+++ Intermediate UI/Intermediate UI/ViewController.swift
@@ -0,0 +1,34 @@
+//
+//  ViewController.swift
+//  Intermediate UI
+//
+//  Created by Adam Israfil on 2/22/18.
+//  Copyright © 2018 Adam Israfil. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController, UIPickerViewDelegate, UIPickerViewDataSource {
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        <#code#>
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        <#code#>
+    }
+    
+    let data:[String] = ["item 1", "item 2", "item 3"]
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
