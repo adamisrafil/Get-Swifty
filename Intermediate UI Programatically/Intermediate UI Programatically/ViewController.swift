bb5d834f8d79d8f0e22fd1505e7a4bac03d8e964
Unknown <adam.israfil@gmail.com>
2018-02-22 14:49:19 -0500

    added ui button and label programatically

--- Intermediate UI Programatically/Intermediate UI Programatically/ViewController.swift
+++ Intermediate UI Programatically/Intermediate UI Programatically/ViewController.swift
@@ -13,6 +13,19 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        let label:UILabel = UILabel(frame: CGRect(x: 20, y: 40, width: 300, height: 20))
+        label.text = "code label"
+        view.addSubview(label)
+        
+        let button:UIButton = UIButton(frame: CGRect(x: 20, y: 100, width: 300, height: 40))
+        button.setTitle("Code Buton", for: .normal)
+        button.backgroundColor = UIColor.lightGray
+        button.addTarget(self, action: #selector(buttonPress), for: .touchUpInside)
+        view.addSubview(button)
+    }
+    
+    @objc func buttonPress() {
+        print("button pressed!!")
     }
 
     override func didReceiveMemoryWarning() {
